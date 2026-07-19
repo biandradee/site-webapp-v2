37625eb4c1fe27619d316e2e4f6c5eb7fed8b88e
Vanilo Ferreira <programacaoti09.vanilo@assectra.com.br>
2026-07-19 12:29:54 -0300

    refactor: Use react-router-dom

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import {
   FooterContainer,
   Nav,
@@ -32,13 +33,13 @@ function Footer() {
     >
       <FooterContainer>
         <Logo>
-          <a href="/">
+          <RouterLink to="/">
             <Image
               src={logoImage}
               alt="Sou Junior - Logo da organização que impulsiona carreiras em tecnologia"
               role="img"
             />
-          </a>
+          </RouterLink>
         </Logo>
         <NavContainer>
           <ContentWrapper>
